7f32d97d8ece2167e3462b60c0d03edd68baa3d3
Annika Lois Dumalogdog <131654335+annekoyya@users.noreply.github.com>
2026-09-19 18:37:03 +0800

    Delete lib directory

--- lib/data/models/game_mode.dart
+++
@@ -1,1 +0,0 @@
-enum GameMode { daily, practice, timed }
--- lib/data/models/letter_status.dart
+++
@@ -1,7 +0,0 @@
-/// Represents the feedback state of a single letter tile after a guess.
-enum LetterStatus {
-  initial, // not yet guessed
-  correct, // green - right letter, right position
-  present, // yellow - right letter, wrong position
-  absent,  // grey - letter not in the word
-}
